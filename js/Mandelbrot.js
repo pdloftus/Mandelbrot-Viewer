e7a69c5e69c9b1da323161f33f0525388b28da03
Peter Loftus <ploftus19@gmail.com>
2015-03-01 23:32:59 -0800

    updated mandelbrot code to scale to the entire canvas, increased canvas size.

--- js/Mandelbrot.js
+++ js/Mandelbrot.js
@@ -1,26 +1,43 @@
 var globals = {};
 
-function generateMandelbrot() {
+function generateMandelbrot(reMin, reMax, imMin, imMax) {
     console.log("generating mandelbrot set");
+    var canvasWidth = globals.canvas.width;
+    var canvasHeight = globals.canvas.height;
     var ctx = globals.canvas.ctx;
 
-    for (var real = -globals.cps; real <= globals.cps; real += globals.delta) {
-        for (var im = -globals.cps; im <= globals.cps; im += globals.delta) {
-            var z = new Complex(0, 0);
-            var c = new Complex(real, im);
+    var scaleX = (reMax - reMin) / canvasWidth;
+    var scaleY = (imMin - imMax) / canvasHeight;
+
+    for (var x = 0; x < canvasWidth; x++) {
+
+        var cRe = (x * scaleX) + reMin;
+
+        for (y = 0; y < canvasHeight; y++) {
+
+            var cIm = (y * scaleY) + imMax;
+
+            var zRe = 0;
+            var zIm = 0;
+
             var inSet = true;
-
             for (var iter = 1; iter <= globals.maxIters; iter++) {
-                z = c.add(z.square());
 
-                if (z.modulus() > globals.escapeVal){
+                var zReSquared = zRe * zRe;
+                var zImSquared = zIm * zIm;
+
+                zIm = (2 * zRe * zIm) + cIm;
+                zRe = zReSquared - zImSquared + cRe;
+
+                if (zReSquared + zImSquared > 4){
                     inSet = false;
                     break;
                 }
             }
 
             if (inSet){
-                ctx.fillRect(real, im, globals.delta, globals.delta);
+                //console.log("painting to x: " + x + ", y: " + y)
+                ctx.fillRect(x, y, 1, 1);
             }
         }
     }
@@ -39,13 +56,16 @@ function initialize() {
     globals.canvas = document.getElementsByTagName('canvas')[0];
     globals.canvas.ctx = globals.canvas.getContext('2d');
     globals.canvas.ctx.fillStyle = "black";
-    globals.cps = 2;
-    globals.maxIters = 300;
-    globals.delta = 0.008;
-    globals.escapeVal = 2;
 
-    initCoordinateSystem();
-    generateMandelbrot();
+    globals.maxIters = 300;
+    globals.reMax = 1.1;
+    globals.reMin = -2.5;
+    globals.imMax = 1.2;
+    globals.imMin = -1.2;
+
+    //initCoordinateSystem();
+    generateMandelbrot(globals.reMin, globals.reMax, 
+                       globals.imMin, globals.imMax);
 }
 
 window.onload = initialize;
