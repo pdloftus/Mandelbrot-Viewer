843b2c48509219aa256463d0be83e86ce2217c77
Peter Loftus <ploftus19@gmail.com>
2015-03-01 22:21:22 -0800

    created initial html, updated mandelbrot generation implementation.

--- js/Mandelbrot.js
+++ js/Mandelbrot.js
@@ -1,27 +1,33 @@
 var globals = {};
 
 function generateMandelbrot() {
+    console.log("generating mandelbrot set");
     var ctx = globals.canvas.ctx;
 
     for (var real = -globals.cps; real <= globals.cps; real += globals.delta) {
         for (var im = -globals.cps; im <= globals.cps; im += globals.delta) {
             var z = new Complex(0, 0);
             var c = new Complex(real, im);
+            var inSet = true;
 
             for (var iter = 1; iter <= globals.maxIters; iter++) {
                 z = c.add(z.square());
 
                 if (z.modulus() > globals.escapeVal){
+                    inSet = false;
                     break;
                 }
             }
 
-            ctx.fillRect(real, im, globals.delta, globals.delta);
+            if (inSet){
+                ctx.fillRect(real, im, globals.delta, globals.delta);
+            }
         }
     }
 }
 
 function initCoordinateSystem() {
+    console.log("initializing coordinate system");
     var ctx = globals.canvas.ctx;
 
     ctx.translate(globals.canvas.width / 2, globals.canvas.height / 2);
@@ -29,6 +35,7 @@ function initCoordinateSystem() {
 }
 
 function initialize() {
+    console.log("initializing");
     globals.canvas = document.getElementsByTagName('canvas')[0];
     globals.canvas.ctx = globals.canvas.getContext('2d');
     globals.canvas.ctx.fillStyle = "black";
@@ -41,4 +48,4 @@ function initialize() {
     generateMandelbrot();
 }
 
-window.onLoad = initialize;
+window.onload = initialize;
